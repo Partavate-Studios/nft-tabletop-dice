4a127c7c41c21d1f1400e284a75e9fcee7c1e1f2
zasKoda <zaskoda@gmail.com>
2022-02-16 11:05:17 -0700

    minor

--- blockchain/contracts/Dice.sol
+++ blockchain/contracts/Dice.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
@@ -22,8 +21,8 @@ contract TabletopDiceNFT is ERC721URIStorage, Ownable {
         returns (uint256)
     {
         _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-        
+
+        uint256 newItemId = _tokenIds.current();        
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
