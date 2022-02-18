45f20910aa587be8616e831074d2e01b676521da
zasKoda <zaskoda@gmail.com>
2022-02-18 16:48:56 -0700

    basic dice

---
+++ client/src/components/_dice.svg.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  props: {
+    isRolling: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: Number,
+      default: 1
+    },
+    type: {
+      type: Number,
+      default: 1
+    },
+    colorTheme: {
+      type: Number,
+      default: 1
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  }
+}
+</script>
+
+<template>
+  <g fill="#ffffff" stroke="#ffffff">
+    <text>{{ value }}</text>
+  </g>
+</template>
+
+<style>
+</style>
