20fe18cc5c41241278fce2743bcc2e3e56fba191
zasKoda <zaskoda@gmail.com>
2022-02-20 00:17:36 -0700

    better error catching

--- client/src/web3dice.js
+++ client/src/web3dice.js
@@ -71,6 +71,27 @@ export const web3dice = {
         params: [{ chainId: chainId }]
       });
     } catch (error) {
+      if (confirm('Chain not found in your wallet. Would you like us to try to add it?')) {
+        let data = [{
+          chainId: chainId,
+          chainName: 'Polygon Testnet',
+          nativeCurrency: {
+            name: 'MATIC',
+            symbol: 'MATIC',
+            decimals: 18
+          },
+          rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
+          blockExplorerUrls: ['https://mumbai.polygonscan.com/']
+        }]
+        try {
+          await window.ethereum.currentProvider.request({
+            method: 'wallet_addEthereumChain',
+            params: data
+          });
+        } catch (error) {
+          alert('We were unable to add the network.')
+        }
+      }
     }
   },
 
@@ -87,8 +108,8 @@ export const web3dice = {
       store.lastRoll[diceId] = roll
       setTimeout(() => {store.isRolling[diceId] = false}, 1000)
       return roll
-    } catch (err) {
-      console.log("Error: ", err)
+    } catch (error) {
+      console.log("Error: ", error)
       store.isRolling[diceId] = false
       return 'failed'
     }
@@ -100,8 +121,8 @@ export const web3dice = {
         store.diceTraits[diceId] = await this.diceContract.getTraits(diceId)
         console.log(store.diceTraits[diceId])
       })
-    } catch (err) {
-      console.log("Error: ", err)
+    } catch (error) {
+      console.log("Error: ", error)
     }
   },
 
@@ -110,8 +131,9 @@ export const web3dice = {
     try {
       store.ownedDice  = await this.diceContract.tokenListOfOwner(store.address)
       await this.getTraits()
-    } catch (e) {
-      console.log("Error: ", err)
+    } catch (error) {
+      store.ownedDice  = []
+      console.log("Error: ", error)
     }
     store.diceLoaded = true
     return store.ownedDice
