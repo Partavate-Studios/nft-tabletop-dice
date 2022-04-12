d65b3a512a735c8331cbc0e01db4252d7ef1c1b9
koda <zaskoda@gmail.com>
2022-04-11 20:28:49 -0700

    more shadow play

--- client/src/components/_die.svg.vue
+++ client/src/components/_die.svg.vue
@@ -94,7 +94,7 @@ export default {
       </radialGradient>
     </defs>
     <g class="diceshadow" :class="{rollshadow: rolling}">
-      <ellipse cx="0" cy="60" rx="80" ry="30" transform="rotate(0)" fill="url('#dieshadow')"  opacity="0.25" stroke-width="0" />
+      <ellipse cx="0" cy="40" rx="60" ry="30" transform="rotate(0)" fill="url('#dieshadow')"  opacity="0.20" stroke-width="0" />
     </g>
     <g class="dice" :class="{rolling: rolling}">
       <g fill="#ffffff" stroke="#ffffff">
