c4310954c287cc1b2da2a02127e74e2941c82561
Arthur Kepler <kepler@partavate.com>
2022-04-20 13:40:15 -0700

    Vite build configuration for Dockerization, Debug

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: './public',
+    emptyOutDir: false,
+    chunkSizeWarningLimit: 1000,
+    // build breaks refs to `this.store` in methods of `SelectedDieBox.vue`. Disabling minify to debug
+    minify: false,
+  },
   plugins: [vue()],
   resolve: {
     alias: {
